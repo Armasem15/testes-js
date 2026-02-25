4926a2d341d5089e62f8395e186ee3f79eb2f4ce
Armasem15 <marcos.walger@escola.pr.gov.br>
2026-02-25 09:00:21 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,15 @@
+console.log
+
+carro1 = "fusca"
+carro2 = "chevete"
+
+kmcarro1 = 100;
+kmcarro2 = 120;
+
+preçokm1 = 
+console.log 
+
+
+//quantos km andou
+//quanto reais vai dar
+// x 
---
+++ notas.js
@@ -0,0 +1,11 @@
+console.log;
+
+aluno="Pedro";
+nota1=6;
+nota2=4;
+nota3=7;
+nota4=6;
+
+media = ((nota1 + nota2 + nota3 + nota4) / 4);
+
+console.log ("o aluno", aluno, "tem a media de", media);
